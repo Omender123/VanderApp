4bb6b6998b8a9ffebfc9e36735d57e190d320992
Omender <Os7290915329@gmail.com>
2021-03-06 23:35:36 +0530

    vender

---
+++ app/src/main/java/com/Vander/Vanderapp/Activity/Add_Product.java
@@ -0,0 +1,18 @@
+package com.Vander.Vanderapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Add_Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__product);
+    }
+
+    public void back(View view) {
+    }
+}
